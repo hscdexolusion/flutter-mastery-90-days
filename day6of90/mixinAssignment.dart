c150ecde0dc866f1f14b12622364d83e0b2f2938
hscdexolusion <tayworgbalestevestan@gmail.com>
2026-05-04 22:24:09 +0000

    changes made to code of day 6 to make up my day 7

--- day6of90/mixinAssignment.dart
+++ day6of90/mixinAssignment.dart
@@ -1,11 +1,48 @@
 mixin Connectable {
-  void connect();
+  void connect() {
+    print('The device is connected');
+  }
 }
 
 mixin Controllable {
-  void control(String action);
+  void control(String action) {
+    print('The device is Controllable');
+  }
 }
 
 mixin Chargeable {
-  void Charge();
+  void Charge() {
+    print('The device is Chargeable');
+  }
 }
+
+class Smartphone with Connectable, Controllable, Chargeable {}
+
+class Laptop with Connectable, Chargeable {
+  void code() {
+    print("This is a Laptop");
+  }
+}
+
+class SmartTV with Connectable, Controllable {
+  void watch(String channel) {
+    print("This is the channel $channel");
+  }
+}
+
+void main() {
+  var s = Smartphone();
+  s.Charge();
+  s.connect();
+  s.control('Volume Up');
+
+  var L = Laptop();
+  L.Charge();
+  L.connect();
+
+  var M = SmartTV();
+  M.connect();
+  M.control('Volume Up');
+}
+
+//
